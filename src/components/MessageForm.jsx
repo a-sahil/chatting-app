c3dc40b342d6fddb128801c665b9a5923234166a
Sahil <sahil2215ali@gmail.com>
2024-05-06 18:23:40 +0530

    final changes

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -47,7 +47,7 @@ const MessageForm = (props) => {
                 onChange={handleUpload}
             />
             <button type="submit" className="send-button"> 
-                <SendOutlined className="send-icon" /> 
+                    <SendOutlined className="send-icon" /> 
             </button>
         </form>
     </div>
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,29 +1,30 @@
-const TheirMessage = (message , lastMessage) =>{
-const isStartingMessage = !lastMessage || lastMessage.sender.username !== message.sender.username;
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
 
-return(
-  <div className="message-row">
-      {isStartingMessage && (
+  return (
+    <div className="message-row">
+      {isFirstMessageByUser && (
         <div
           className="message-avatar"
           style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
         />
       )}
-     {message.attachments && message.attachments.length > 0
+      {message.attachments && message.attachments.length > 0
         ? (
           <img
             src={message.attachments[0].file}
             alt="message-attachment"
             className="message-image"
-            style={{ marginLeft: isStartingMessage ? '4px' : '48px' }}
+            style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
           />
         )
         : (
-          <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isStartingMessage ? '4px' : '48px' }}>
+          <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
             {message.text}
           </div>
         )}
     </div>
   );
 };
+
 export default TheirMessage;
